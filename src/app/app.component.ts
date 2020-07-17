4ce1c9f6a7c7400b4453a9884a5754e850ffe6c9
wooj <zomdar@gmail.com>
2020-07-16 23:54:01 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faGlobeAmericas, faPeace, faFistRaised } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ppp-ui';
+  faGlobeAmericas = faGlobeAmericas;
+  faPeace = faPeace;
+  faFistRaised = faFistRaised
+
+  isNavbarCollapsed = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { StatesComponent } from './states/states.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BusinessInfoComponent } from './business-info/business-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatesComponent,
+    HomeComponent,
+    AboutComponent,
+    BusinessInfoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
